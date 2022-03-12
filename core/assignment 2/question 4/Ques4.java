adb8f0e35349df0cf5128cce1adfdf7b4054c3b3
radhika456 <101452661+radhika456@users.noreply.github.com>
2022-03-12 17:51:50 +0530

    Create Ques4.java

---
+++ core/assignment 2/question 4/Ques4.java
@@ -0,0 +1,10 @@
+
+
+public class Ques4 {
+
+	public static void main(String[] args) {
+		Employee employee=new
+				CommissionEmployee(100, "rahul", 2.1, 3000000);
+		System.out.println(employee.getPayment());
+	}
+}
