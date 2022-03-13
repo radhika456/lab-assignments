991db78c816310f3e68726ff49746771901baf22
radhika456 <101452661+radhika456@users.noreply.github.com>
2022-03-13 14:33:52 +0530

    Create AssQ3.java

---
+++ core/assignment 2/question 3/AssQ3.java
@@ -0,0 +1,15 @@
+
+
+public class AssQ3{
+
+	public static void main(String[] args) {
+		
+		Account account=new
+				SavingsAccount("mahek", "12123456", 30000, 5, 1000);
+	
+		
+		Account account2=new 
+			CurrentAccount("komal", "544562135", 500000, "AB1234", 50000);
+		
+	}
+}
