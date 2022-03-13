02d6e516965625b6e60de19d61313c5436926f07
radhika456 <101452661+radhika456@users.noreply.github.com>
2022-03-13 14:42:11 +0530

    Create AssQ4.java

---
+++ core/assignment 2/question 4/AssQ4.java
@@ -0,0 +1,10 @@
+
+
+public class AssQ4 {
+
+	public static void main(String[] args) {
+		Employee employee=new
+				CommissionEmployee(101, "payal", 2.1, 2000000);
+		System.out.println(employee.getPayment());
+	}
+}
