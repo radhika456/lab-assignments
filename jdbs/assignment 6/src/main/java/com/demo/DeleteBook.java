82c33388f8a2f08f22d4d9b2b93ba518148bca8e
radhika456 <101452661+radhika456@users.noreply.github.com>
2022-03-22 12:21:04 +0530

    Create DeleteBook.java

---
+++ jdbs/assignment 6/src/main/java/com/demo/DeleteBook.java
@@ -0,0 +1,46 @@
+package com.demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeleteBook {
+	public static void main(String[] args) {
+		//1. u need to load the driver
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("driver is loaded..");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		PreparedStatement stmt=null;
+		Connection connection=null;
+		
+		try {
+			 connection=DriverManager.getConnection
+					("jdbc:mysql://localhost:3306/bookstore", "book", "book");
+			
+			stmt=connection.
+					prepareStatement("delete from book where book_id=?");
+			
+			stmt.setInt(1, 10005);
+		
+			int noOfRowsEffected= stmt.executeUpdate();
+			System.out.println(noOfRowsEffected);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if(connection!=null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		
+	}
+}
