9e22c8c5bceed126211b67c313ff0d5e18344d09
radhika456 <101452661+radhika456@users.noreply.github.com>
2022-03-22 12:55:32 +0530

    Create BiggestOne.java

---
+++ core/assignment 4/question3/BiggestOne.java
@@ -0,0 +1,43 @@
+package Assignment4.ques3;
+
+import java.io.*;
+import java.util.*;
+
+public class DisplayBiggestOne
+{
+	BufferedReader br;
+	private ArrayList<Double> doubles=new ArrayList<>();
+	
+	public DisplayBiggestOne()
+	{
+	init();	
+	}
+	
+	void init()
+	{
+		try {
+			double doubleValue;
+			br= new BufferedReader(new FileReader(new File("C:/Users/dell/Documents/data.txt")));
+			String str = null;
+			while ((str = br.readLine()) != null) {
+			doubleValue=Double.parseDouble(str);
+			doubles.add(doubleValue);
+			}
+			
+			double biggestOne = 0;
+			for(int i=1;i<doubles.size();i++)
+			{
+				biggestOne=doubles.get(0);
+				if(biggestOne<=doubles.get(i))	biggestOne=doubles.get(i);
+			}
+			System.out.println("Biggest One : "+biggestOne);
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		
+	}
+}
