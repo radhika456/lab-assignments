863588621b357913689a3b673a609f7db2a29d7d
radhika456 <101452661+radhika456@users.noreply.github.com>
2022-03-26 12:38:52 +0530

    Create BookService.java

---
+++ Assignment 9/spring DI/src/main/java/com/bookapp/modal/service/BookService.java
@@ -0,0 +1,10 @@
+package com.bookapp.model.service;
+
+import java.util.List;
+
+import com.bookapp.model.dao.Book;
+
+public interface BookService {
+	public List<Book> getAll();
+	public void addBook(Book b);
+}
