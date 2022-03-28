3acdbca63164caaa37967563e0c5a16ff07d4345
radhika456 <101452661+radhika456@users.noreply.github.com>
2022-03-28 12:24:33 +0530

    Create Calculator.java

---
+++ assignment 5/src/main/java/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package calculator;
+
+public class Calculator {
+	public int add(int a, int b) {
+		return a+b;
+	}
+  public int sub(int a,int b) {
+    return a-b;
+  }
+  
+	public int multiply(int a, int b) {
+		return a*b;
+	}
+
+	public double divide(double a, double b) {
+		if(b==0)
+		{
+			throw new IllegalArgumentException("Number cannot be divided by zero");
+		}
+		return a/b;
+	}
+}
